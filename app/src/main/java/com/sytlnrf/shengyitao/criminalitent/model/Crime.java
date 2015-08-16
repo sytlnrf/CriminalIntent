eda0a9112d18f63577c588df59433d24fc25cc73
sytlnrf <sytlnrf@gmail.com>
2015-08-16 16:43:52 +0800

    create

--- app/src/main/java/com/sytlnrf/shengyitao/criminalitent/model/Crime.java
+++ app/src/main/java/com/sytlnrf/shengyitao/criminalitent/model/Crime.java
@@ -6,5 +6,21 @@ import java.util.UUID;
  * Created by shengyitao on 8/16/15.
  */
 public class Crime {
-    private UUID uid;
+    private UUID mId;
+    private String mTitle;
+    public Crime(){
+        this.mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return this.mId;
+    }
+
+    public String getmTitle() {
+        return this.mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
 }
